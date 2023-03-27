8d738ba01787106ed47d72318b548e1a04e3297d
vncouver <61960395+vncouver@users.noreply.github.com>
2023-03-27 10:19:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write('''
 ##### *Resume* 
 ''')
 
-image = Image.open('dp.png')
+image = Image.open('IMG_0485.jpeg')
 st.image(image, width=150)
 
 st.markdown('## Summary', unsafe_allow_html=True)
