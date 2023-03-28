9e878e87b4a9b158acb0daaefd467a45a112d83a
vncouver <61960395+vncouver@users.noreply.github.com>
2023-03-28 13:31:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://www.linkedin.com/in/merterkansozen" target="_blank">Mert Erkan SÖZEN</a>
+  <a class="navbar-brand" href="https://www.linkedin.com/in/merterkansozen" target="_blank">Mert Erkan SÖZEN 📫</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
